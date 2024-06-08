ac3d2ac127b2658642b4dd40045f53213ba5943d
bank2783 <117621142+bank2783@users.noreply.github.com>
2024-06-08 10:24:17 +0530

    comit

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,13 @@ use App\Http\Middleware\checkLogin;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/login',[AuthController::class,'loginView'])->name('login_view');
 
 Route::middleware([checkLogin::class])->group(function () {
     Route::get('show-roster',[HomeController::class,'showroster'])->name('show_roster_view');
-    Route::get('roster-management',[HomeController::class,'showRosterManagementPage'])->name('show_roster_management_view');
+    Route::get('/',[HomeController::class,'showRosterManagementPage'])->name('show_roster_management_view');
     Route::post('store-roster',[RosterController::class,'storeRoster'])->name('store_roster');
     Route::post('edit-roster/{id}',[RosterController::class,'editRoster'])->name('editRoster');
     Route::post('delete-roster/{id}',[RosterController::class,'destroyRoster'])->name('delete_roster');
